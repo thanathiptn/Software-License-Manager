5654c694d9eb421a42c472bd2cbe4d3bfa8a78c0
thanathiptn <thanathip.tn@gmail.com>
2026-08-21 11:21:43 +0000

    Disable task route and set API base URL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  baseURL: 'https://friendly-tribble-xr5wwwjrvrj9hpwv4-5001.app.github.dev',
   //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
