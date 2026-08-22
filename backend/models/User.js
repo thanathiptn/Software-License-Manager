2ec49ac0b96745e05e6798a0de1903e231b4e1b1
thanathip <thanathip.tn@gmail.com>
2026-08-22 12:30:43 +1000

    SLM-2 add role field to user model and admin only middleware

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: { type: String, enum: ['admin','user'], default: 'user' },
     university: { type: String },
     address: { type: String },
 });
@@ -14,6 +15,7 @@ userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 module.exports = mongoose.model('User', userSchema);
