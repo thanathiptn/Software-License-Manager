af306d6d4d3768688d679f9dabe739c9f2172c51
Thanathip N <thanathip.tn@gmail.com>
2026-08-30 14:52:49 +0000

    Set axiosConfig baseURL to EC2 IP for deployment

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001',
+  baseURL: 'http://3.27.248.38:5001',
   headers: { 'Content-Type': 'application/json' },
 });
 
